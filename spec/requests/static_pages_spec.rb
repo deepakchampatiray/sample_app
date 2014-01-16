515a7e07ff699abdddaafe1b1c9dc7c5f02173c4
Deepak Champatiray <deepakchampatiray@gmail.com>
2014-01-16 00:07:21 -0500

    Added links in Header file and updated static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,53 +2,30 @@ require 'spec_helper'
 
 describe "StaticPages" do
   let(:base_title) {"Ruby On Rails Tutorial Sample App"}
+  subject { page }
 
   describe "Home page" do
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    before {visit root_path}
 
-    it 'should have a title ending with "Home"' do
-      visit root_path
-      page.should have_selector('title', :text => "#{base_title}")
-    end
+    it { should have_selector('h1', :text => 'Sample App') }
+    it { should have_selector('title', :text => "#{base_title}") }
   end
 
   describe "Help Page" do
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-    end
-
-    it 'should have a title ending with "Help"' do
-      visit help_path
-      page.should have_selector('title', :text => "#{base_title} | Help")
-    end
+    before {visit help_path}
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('title', :text => "#{base_title} | Help") }
   end
 
   describe "About Us Page" do
-    it "should have the h1 'About'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it 'should have a title ending with "About Us"' do
-      visit about_path
-      page.should have_selector('title', :text => "#{base_title} | About Us")
-    end
+    before {visit about_path}
+    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_selector('title', :text => "#{base_title} | About Us") }
   end
 
   describe "Contact Us Page" do
-    it "should have a h1 with 'Contact Us'" do 
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact Us')
-    end  
-
-    it "should have a title with text ending with 'Contact Us'" do
-      ## See how brackets can be used as well.
-      visit (contact_path);
-      page.should(have_selector('title', :text => "#{base_title} | Contact Us"));
-    end
+    before {visit contact_path}
+    it { should have_selector('h1', :text => 'Contact Us') }
+    it { should(have_selector('title', :text => "#{base_title} | Contact Us")); }
   end
 end
