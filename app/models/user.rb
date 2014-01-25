58b6cdb4b2abc1662f11b6d51826a6bea0a77a6c
Deepak Champatiray <deepakchampatiray@gmail.com>
2014-01-25 01:02:33 -0500

    Added Name and Email Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  VALID_EMAIL_ADDERSS = /\A[\w+\-\.]+@[a-z]+\.([a-z]+\.)*[a-z]+\z/i;
+
+  validates(:name, {presence: true, length: {maximum: 40}})
+  validates(	:email, 
+  				{
+  					presence: {message: 'is required.'}, 
+  					format: {with: VALID_EMAIL_ADDERSS, message: 'is not valid.', on: :create},
+  					uniqueness: {case_sensitive: false}
+  				}
+  			);
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,62 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before {@user = User.new({name: 'deepak', email: 'deepak@gmail.com'})}
+
+  subject { @user }
+
+  it {should respond_to(:name)}
+  it {should respond_to(:email)}
+  it {should be_valid}
+
+  describe "when name not present" do
+  	before {@user.name =""}
+
+  	it {should_not be_valid}
+  end
+
+  describe "when name is too long" do
+  	before {@user.name = "a" * 41}
+
+  	it {should_not be_valid}
+  end
+
+  describe "when email not present" do
+  	before {@user.email = ""}
+
+  	it {should_not be_valid}
+  end
+
+  describe "when email is not valid" do
+    before {@user.email = "deepak.com"}
+
+    it {should_not be_valid}
+  end
+
+  describe "when email is valid" do
+    before {@user.email = "deepakchampatiray@gmail.com"}
+
+    it{should be_valid}
+  end
+
+  describe "when email is not unique" do
+    before do
+      duplicate_user = @user.dup;
+      @user.email = @user.email.upcase;
+      duplicate_user.save;
+    end
+    
+    it{should_not be_valid}
+  end
 end
